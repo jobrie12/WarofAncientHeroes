94a647c6b8288918a8b85f50f22a364688a77935
jobrie12 <jamesbryantobrien@gmail.com>
2015-02-24 17:32:51 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+angular
+    .module('WarOfAncientHeroes',[])
+    .controller('gameCtrl',function($scope, LoadHeroesService, $http,  $log){
+        var init = function(){
+
+            $scope.heroes = [];
+            LoadHeroesService.fetch().then(function(data) {
+                $scope.heroes= data.heroes;
+                console.log($scope.heroes);
+            })
+            $scope.hero = null;
+            //$scope.heroes = [{"name":"BRBRB"},{"name":"KLKLKLK"}];
+        }
+
+        init();
+    })
+    .factory('LoadHeroesService', function($q,$timeout,$http) {
+        var heroes = {
+            fetch: function(callback) {
+                var deferred = $q.defer();
+                $timeout(function() {
+                    $http.get("data.json").success(function(data) {
+                        deferred.resolve(data);
+                    });
+                }, 30);
+
+                return deferred.promise;
+            }
+        };
+
+        return heroes;
+    });
